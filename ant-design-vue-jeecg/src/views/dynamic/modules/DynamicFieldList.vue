8210ff5ecc1d9962f3b49af74b7aa109c4593477
seavers <seavers@163.com>
2022-05-15 23:54:37 +0800

    动态表格，字段列表支持拖拽排序，第一版

--- ant-design-vue-jeecg/src/views/dynamic/modules/DynamicFieldList.vue
+++ ant-design-vue-jeecg/src/views/dynamic/modules/DynamicFieldList.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="dynamic-field-list">
     <a-list :data-source="columns">
-      <a-list-item slot="renderItem" slot-scope="item, index">
-        <a-icon class="dragbar" type="menu" />
-        <span class="title">{{ item.title }}</span>
-        <a-switch size="small" :checked="settingColumns.indexOf(item.dataIndex)>-1" @change="onChangeFieldShow" :v-data-index="item.dataIndex"/>
+      <a-list-item slot="renderItem" slot-scope="item, index" draggable="true" @dragstart.native="dragStart($event, item)" @dragover="dragOver($event)" @dragend.native="dragEnd($event)">
+        <div class="dynamic-field-item-wrap">
+          <a-icon class="dragbar" type="menu" />
+          <span class="title">{{ item.title }}</span>
+          <a-switch size="small" :checked="settingColumns.indexOf(item.dataIndex)>-1" @change="onChangeFieldShow" :v-data-index="item.dataIndex"/>
+        </div>
       </a-list-item>
       <p><a-button icon="plus">新增字段</a-button></p>
     </a-list>
@@ -18,6 +20,7 @@
     name: "DynamicFieldList",
     data () {
       return {
+        dragging: null
       }
     },
     props: {
@@ -36,30 +39,118 @@
 
         this.$emit("change", this.settingColumns)
       },
+
+
+      dragStart(e) {
+        //console.log("drop-start", e, e.target);
+        this.dragging = e.target;
+
+        //e.preventDefault();
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', e.target.innerHTML);
+
+        document.querySelector('.dynamic-field-list').classList.add("dynamic-field-list-sorting")
+        e.target.parentNode.childNodes.forEach(function(el, index) {
+          el.style = 'position:relative';
+          el.index = index;
+        })
+
+        setTimeout(() => {
+          e.target.firstChild.style="visibility: hidden";
+        }, 10);
+      },
+      dragOver(e) {
+        //e.preventDefault();
+        //console.log(this.dragging.innerText, e.currentTarget.innerText, e.currentTarget.nextSibling.innerText);
+        this.dragTarget = e.currentTarget;
+
+        var target = e.currentTarget;
+        if(target.parentNode == this.dragging.parentNode && target != this.dragging) {
+          if (this.dragging.index > target.index) {
+            target.firstChild.style = "position:relative;top: " + (this.dragging.clientHeight) + "px"
+          } else if (this.dragging.index < target.index){
+            target.firstChild.style = "position:relative;top: -" + (this.dragging.clientHeight) + "px"
+          }
+        }
+
+        e.preventDefault();
+      },
+      dragEnd(e) {
+        console.log("drag-end", this.dragging, this.dragTarget);
+        this.dragging.firstChild.style.cssText = "visibility: visible";
+        document.querySelector('.dynamic-field-list').classList.remove("dynamic-field-list-sorting")
+        this.dragging.parentNode.childNodes.forEach(function(el, index) {
+          el.firstChild.style.cssText = 'position:relative;top:0px';
+        })
+
+        if (this.dragging === this.dragTarget) {
+          return ;
+        }
+        if (this.dragging.index < this.dragTarget.index) {
+          this.dragging.parentNode.insertBefore(this.dragging, this.dragTarget.nextSibling)
+        } else {
+          this.dragging.parentNode.insertBefore(this.dragging, this.dragTarget)
+        }
+
+      },
+
+
+//         let targetTop = e.target.getBoundingClientRect().top
+//         let dragingTop = this.getBoundingClientRect().top
+// //      console.log('drag move', targetTop)
+//         if(this.target.nodeName==='LI'&&this.target !== this.draging) {
+//           if(this.target&&this.target.animated) {
+//             return
+//           }
+//           let targetIndex = this.target.getAttribute('index')
+//           let dragingIndex = this.draging.getAttribute('index')
+//           if(targetIndex > dragingIndex) {//拖拽元素往下移动
+//             //target的下一个元素
+//             this.target.parentNode.insertBefore(this.draging, this.target.nextSibling)
+//           }else{
+//             this.target.parentNode.insertBefore(this.draging, this.target)
+//           }
+//           this._animation(targetTop, this.target)
+//           this._animation(dragingTop, this.draging) 
+//         }
+//      }
     }
   }
 </script>
 
 
 <style scoped>
-  .dynamic-field-list .ant-list-item {
+.dynamic-field-list .ant-list-item {
     width: 200px;
     border-bottom: none;
+    padding: 0;
+  }
+  .dynamic-field-list .dynamic-field-item-wrap {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
     padding: 4px 4px;
   }
-  .dynamic-field-list .ant-list-item .dragbar {
+  .dynamic-field-list-sorting .dynamic-field-item-wrap {
+    transition: top 0.36s;
+  }
+  .dynamic-field-list .dynamic-field-item-wrap .dragbar {
     font-size: 16px;
     padding: 4px;
     cursor: pointer;
   }
-  .dynamic-field-list .ant-list-item .title {
+  .dynamic-field-list .dynamic-field-item-wrap .title {
     margin-left: 4px;
     flex-grow: 1;
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
   }
-  .dynamic-field-list .ant-list-item:hover {
+  .dynamic-field-list .dynamic-field-item-wrap .ant-switch {
+    transition: none;
+    -webkit-transition: none;
+  }
+  .dynamic-field-list .dynamic-field-item-wrap:hover {
     background: #eee;
   }
 </style>
