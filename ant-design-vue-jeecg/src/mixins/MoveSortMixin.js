70cbd27079f46b969391cb56d8ba9cda92e306fd
seavers <seavers@163.com>
2022-05-18 20:51:48 +0800

    优化拖拽的能力，优化体验

--- ant-design-vue-jeecg/src/mixins/MoveSortMixin.js
+++ ant-design-vue-jeecg/src/mixins/MoveSortMixin.js
@@ -7,7 +7,7 @@ export const MoveSortMixin = {
   methods:{
     mouseDown(e) {
       //e.preventDefault();
-      console.debug("drag-start", e, e.target);
+      console.debug("drag-start", e);
       this.dragging = this.bubble(e.target);
       this.dragList = this.dragging.parentNode;
       
@@ -32,88 +32,8 @@ export const MoveSortMixin = {
         document.removeEventListener('mousemove', this.mouseMove);
         return ;
       }
-      //console.log("mouse-move", e, this.dragging.index);
-      var clientHeight = this.dragging.clientHeight;
-      var maxIndex = this.dragList.clientHeight/clientHeight;
       
       var offsetY = this.offsetEvent(e, this.dragList);
-      this.changePosition(offsetY, e);
-      
-      //需要阻止冒泡，这样有drop事件，表示拖拽成功
-      e.preventDefault();
-    },
-    dragOver(e) {
-      if(this.dragging != null) {
-        e.preventDefault();
-      }
-    },
-    dragLeave(e) {
-      //console.log("drag-leave", this.dragging.index, e.currentTarget.index );
-    },
-    mouseUp(e) {
-      document.removeEventListener('mousemove', this.mouseMove);
-      document.removeEventListener('mouseup', this.mouseUp);
-      if(this.dragging == null) {
-        return ;
-      }
-      
-      console.debug("drag-end", e, this.dragging, this.dragTargetIndex);
-      this.dragging.style.cssText = "opacity:0;z-index:0";
-      this.dragging.classList.remove("dragging")
-      this.dragList.classList.remove("dynamic-field-list-sorting")
-      this.dragList.childNodes.forEach(function(el, index) {
-        el.style.cssText = 'position:relative;top:0px';
-      })
-      
-      var dragTarget = this.dragging.parentNode.childNodes[this.dragTargetIndex];
-      if (this.dragging === dragTarget) {
-        this.dragging = null;
-        return ;
-      }
-      
-      
-      //计算dragTargetIndex
-      var dragging = this.dragging;
-      var clientHeight = dragging.clientHeight;
-      var offsetY = this.offsetEvent(e, this.dragList);
-      
-      var draggingIndex = -1;
-      var targetIndex = -1;
-      var children = this.dragging.parentNode.childNodes;
-      for(var i = 0; i < children.length; i++) {
-        var el = children[i];
-        if(el.dragTop < dragging.dragTop) {
-          var base = el.dragTop + clientHeight/2;
-          if (offsetY < base) {
-            targetIndex = i;
-            break;
-          }
-        } else if(el.dragTop > dragging.dragTop) {
-          var base = el.dragTop - clientHeight/2;
-          if (offsetY > base) {
-            targetIndex = i;
-          }
-        }
-      }
-      
-      for(var i = 0; i < children.length; i++) {
-        var el = children[i];
-        if(el == dragging) {
-          draggingIndex = i;
-        }
-      }
-      
-      if (draggingIndex != -1 && targetIndex != -1) {
-        this.moveSortIndex(draggingIndex, targetIndex)
-      } else {
-        console.error("drag index error", draggingIndex, targetIndex);
-      }
-      
-      this.dragging = null;
-    },
-    
-    
-    changePosition(offsetY, e) {
       //var targetIndex = target.index;
       var dragging = this.dragging;
       var clientHeight = this.dragging.clientHeight;
@@ -133,14 +53,14 @@ export const MoveSortMixin = {
         }
         
         if(el.dragTop < dragging.dragTop) {
-          var base = el.dragTop + clientHeight/2;
+          var base = el.dragTop + clientHeight;
           if (offsetY < base) {
             el.style = "position:relative;top: " + (clientHeight) + "px";
           } else {
             el.style = "position:relative;top: 0px";
           }
         } else if(el.dragTop > dragging.dragTop) {
-          var base = el.dragTop - clientHeight/2;
+          var base = el.dragTop;
           if (offsetY > base) {
             el.style = "position:relative;top: " + (-clientHeight) + "px";
           } else {
@@ -150,7 +70,80 @@ export const MoveSortMixin = {
           el.style = "position:relative;top: 0px";
         }
       });
+      
+      //需要阻止冒泡，这样有drop事件，表示拖拽成功
+      e.preventDefault();
     },
+    dragOver(e) {
+      if(this.dragging != null) {
+        e.preventDefault();
+      }
+    },
+    dragLeave(e) {
+      //console.log("drag-leave", this.dragging.index, e.currentTarget.index );
+    },
+    mouseUp(e) {
+      document.removeEventListener('mousemove', this.mouseMove);
+      document.removeEventListener('mouseup', this.mouseUp);
+      if(this.dragging == null) {
+        return ;
+      }
+      
+      this.dragging.style.cssText = "opacity:0;z-index:0";
+      this.dragging.classList.remove("dragging")
+      this.dragList.classList.remove("dynamic-field-list-sorting")
+      this.dragList.childNodes.forEach(function(el, index) {
+        el.style.cssText = 'position:relative;top:0px';
+      })
+      
+      //计算dragTargetIndex
+      var dragging = this.dragging;
+      var clientHeight = dragging.clientHeight;
+      var offsetY = this.offsetEvent(e, this.dragList);
+      
+      var draggingIndex = -1;
+      var targetIndex = -1;
+      var children = this.dragging.parentNode.childNodes;
+      for(var i = 0; i < children.length; i++) {
+        var el = children[i];
+        if(el.dragTop < dragging.dragTop) {
+          var base = el.dragTop + clientHeight;
+          if (offsetY < base) {
+            targetIndex = i;
+            break;
+          }
+        } else if(el.dragTop > dragging.dragTop) {
+          var base = el.dragTop;
+          if (offsetY > base) {
+            targetIndex = i;
+          }
+        }
+      }
+      
+      for(var i = 0; i < children.length; i++) {
+        var el = children[i];
+        if(el == dragging) {
+          draggingIndex = i;
+        }
+      }
+
+            
+      console.info("drag-end", draggingIndex, targetIndex);
+      var dragTarget = this.dragging.parentNode.childNodes[targetIndex];
+      if (this.dragging === dragTarget) {
+        this.dragging = null;
+        return ;
+      }
+      
+      if (draggingIndex != -1 && targetIndex != -1) {
+        this.moveSortIndex(draggingIndex, targetIndex)
+      } else {
+        console.error("drag index error", draggingIndex, targetIndex);
+      }
+      
+      this.dragging = null;
+    },
+    
     bubble(el) {
       while(el != null) {
         if(el.tagName == 'LI' || el.classList.contains("ant-row") || el.classList.contains("ant-row-flex")) {
@@ -158,9 +151,6 @@ export const MoveSortMixin = {
         }
         el = el.parentNode
       }
-    },
-    offset(el, base) {
-      return el.getBoundingClientRect().top - base.getBoundingClientRect().top;
     },
     offsetEvent(e, base) {
       return e.clientY - base.getBoundingClientRect().top;
